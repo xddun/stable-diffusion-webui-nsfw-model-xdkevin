1b29ac5204956d53231497ecd5c99b572bae116d
jovijovi <mageyul@hotmail.com>
2023-03-17 20:17:03 +0800

    feat: NSFW filter adjustment & warning image

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-A NSFW checker for [Web UI](https://github.com/AUTOMATIC1111/stable-diffusion-webui). Replaces non-worksafe images with black squares. Install it from UI.
+# Adjustable NSFW filter
+
+:construction:
+An adjustable NSFW filter for [Web UI](https://github.com/AUTOMATIC1111/stable-diffusion-webui). Replaces non-worksafe images with black squares. Install it from UI.
+
+## Features
+
+- Customize NSFW filter adjustment
+- Customize warning image
--- scripts/censor.py
+++ scripts/censor.py
@@ -1,13 +1,21 @@
-import torch
-from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
-from transformers import AutoFeatureExtractor
-from PIL import Image
+import os.path
 
-from modules import scripts, shared
+import numpy as np
+import torch
+from PIL import Image
+from diffusers.utils import logging
+from scripts.safety_checker import StableDiffusionSafetyChecker
+from transformers import AutoFeatureExtractor
+
+from modules import scripts
+
+logger = logging.get_logger(__name__)
 
 safety_model_id = "CompVis/stable-diffusion-safety-checker"
 safety_feature_extractor = None
 safety_checker = None
+
+warning_image = os.path.join("extensions", "stable-diffusion-webui-nsfw-filter", "warning", "warning.png")
 
 
 def numpy_to_pil(images):
@@ -31,7 +39,11 @@ def check_safety(x_image):
         safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
 
     safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
-    x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
+    x_checked_image, has_nsfw_concept = safety_checker(
+        images=x_image,
+        clip_input=safety_checker_input.pixel_values,
+        safety_checker_adj=0,  # TODO: customize adjustment
+    )
 
     return x_checked_image, has_nsfw_concept
 
@@ -40,6 +52,23 @@ def censor_batch(x):
     x_samples_ddim_numpy = x.cpu().permute(0, 2, 3, 1).numpy()
     x_checked_image, has_nsfw_concept = check_safety(x_samples_ddim_numpy)
     x = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
+
+    index = 0
+    for unsafe_value in has_nsfw_concept:
+        try:
+            if unsafe_value is True:
+                hwc = x.shape
+                print(f"##   warning_image={warning_image}  ##")
+                y = Image.open(warning_image).convert("RGB").resize((hwc[3], hwc[2]))
+                y = (np.array(y) / 255.0).astype("float32")
+                y = torch.from_numpy(y)
+                y = torch.unsqueeze(y, 0).permute(0, 3, 1, 2)
+                assert y.shape == x.shape
+                x[index] = y
+            index += 1
+        except Exception as e:
+            logger.warning(e)
+            index += 1
 
     return x
 
